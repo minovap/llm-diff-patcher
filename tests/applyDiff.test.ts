8ae9f19771162898654c2bd66f9a6d52e2b5a938
Olcay Konuskan <olcay@konuskan.se>
2025-03-03 22:53:36 +0100

    wip

--- tests/applyDiff.test.ts
+++ tests/applyDiff.test.ts
@@ -1,46 +1,10 @@
 import { parsePatch } from "../src/utils/parsePatch";
-import fs from 'fs';
-import path from 'path';
 import * as Diff from "diff";
 import { NotEnoughContextError} from "../src/utils/errors";
 import {applyDiff} from "../src/utils/applyDiff";
-import {applyPatchToFiles} from "../src";
 
-const testDir = path.join(__dirname, 'applyDiffFiles');
 
 describe('applyDiff', () => {
-  beforeEach(() => {
-    // Clean up any result files before running tests
-    if (fs.existsSync(testDir)) {
-      const files = fs.readdirSync(testDir);
-      for (const file of files) {
-        if (file.endsWith('.result.txt')) {
-          fs.unlinkSync(path.join(testDir, file));
-        }
-      }
-    }
-  });
-
-  it('should open files and edit them', () => {
-    const patch = `--- a/test1.originaltext.txt
-+++ b/test1.result.txt
-@@ -1,4 +1,4 @@
- Hello world
--This is a test file
-+This is a modified test file
- It contains multiple lines
- Goodbye world`;
-
-    const result = applyPatchToFiles(patch, {
-      basePath: testDir,
-    });
-
-    const expectedResultContent = fs.readFileSync('tests/applyDiffFiles/test1.expectedresult.txt', 'utf8');
-    const resultContent = fs.readFileSync('tests/applyDiffFiles/test1.result.txt', 'utf8');
-
-    expect(resultContent).toEqual(expectedResultContent);
-  });
-
   it('should handle multiple hunks in a single diff', () => {
     const patch = `--- a/test1.originaltext.txt
 +++ b/test1.originaltext.txt
@@ -57,31 +21,6 @@ describe('applyDiff', () => {
     
     expect(diffsGroupedByFilenames.length).toBe(1);
     expect(diffsGroupedByFilenames[0].diffs.length).toBe(2);
-  });
-
-  it('should handle errors when file not found', () => {
-    const patch = `--- a/nonexistent.txt
-+++ b/nonexistent.txt
-@@ -1,4 +1,4 @@
- Hello world
--This is a test file
-+This is a modified test file
- It contains multiple lines
- Goodbye world`;
-
-    const diffsGroupedByFilenames = parsePatch(patch);
-
-    for (const diffGroup of diffsGroupedByFilenames) {
-      const oldFilePath = path.join(testDir, diffGroup.oldFileName);
-      
-      expect(() => {
-        try {
-          const fileContents = fs.readFileSync(oldFilePath, 'utf8');
-        } catch (e) {
-          throw Error(`old file ${oldFilePath} not found`);
-        }
-      }).toThrow(`old file ${oldFilePath} not found`);
-    }
   });
 
   it('should throw an error when hunk counts mismatch', () => {
